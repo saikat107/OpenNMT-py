cf6c546d5f3e421c4d46344ea3cca1a5c02f5bc5
Saikat Chakraborty <saikatc@virginia.edu>
2019-03-06 23:28:31 -0500

    Option Parser added in full translator

---
+++ codit/codit_options_parser.py
@@ -0,0 +1,37 @@
+import argparse
+
+import onmt
+
+
+def get_structure_transformation_parser():
+    parser_structure = argparse.ArgumentParser(
+        description='full_translation.py',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    onmt.opts.add_md_help_argument(parser_structure)
+    onmt.opts.translate_opts(parser_structure)
+    parser_structure.add_argument('--grammar', help='Path of the grammar file', required=True)
+    parser_structure.add_argument('-tmp_file', default='tmp/generated_node_types.nt')
+    return parser_structure
+
+
+def get_token_transformation_parser():
+    parser = argparse.ArgumentParser(
+        description='translate_token.py',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    onmt.opts.add_md_help_argument(parser)
+    onmt.opts.translate_opts(parser)
+    parser.add_argument('--name', help='Name of the Experiment')
+    return parser
+
+
+def get_options(options):
+    structure_options = get_structure_transformation_parser().parse_args(
+        (' -src ' + options.src_struct
+         + ' -model ' + options.model_structure + ' -beam_size 100 -n_best 100 -gpu 0 '
+         + ' --grammar ' + options.grammar).split())
+    token_options = get_token_transformation_parser().parse_args(
+        ('-model ' + options.model_token + ' -src ' + options.src_token
+         + ' --name ' + options.name + ' -batch_size 1 ' + ' -beam_size ' + str(options.beam_size)
+         + ' - n_best ' + str(options.n_best))
+            .split())
+    return structure_options, token_options
--- util.py
+++ util.py
@@ -1,19 +1,23 @@
 import inspect
 import os
+from datetime import datetime
 
 def debug(*msg):
+    time = datetime.now()
+    timestr = str(time.strftime('%X'))
     import inspect
     file_path = inspect.stack()[1][1]
     line_num = inspect.stack()[1][2]
     file_name = file_path
     if os.getcwd() in file_path:
         file_name = file_path[len(os.getcwd())+1:]
-    stack = str(file_name) + ' # ' + str(line_num)
+    stack = str(file_name) + '#' + str(line_num) + ' [' + timestr + ']'
     print(stack, end=' ')
     res = '\t'
     for ms in msg:
         res += (str(ms) + ' ')
     print(res)
+
 
 def write_dummy_generated_node_types(input_file, output_file):
     with open(input_file) as inp:
